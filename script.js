3475d9c0b37f5dba70a6c9bcdd966f0e9d94365b
Ramesh rai <111167345+rameshrai98125@users.noreply.github.com>
2024-01-25 09:40:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function locomotiveFun() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotiveFun();
+
+Shery.mouseFollower();
+
+Shery.hoverWithMediaCircle(".rai" /* Element to target.*/, {
+  images: [
+    "https://images.unsplash.com/photo-1705864864158-4c583edee4e3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8",
+  ] /*OR*/,
+});
+
+Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+var menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener("click", function () {
+  sidebar.classList.add("openbar");
+});
+close.addEventListener("click", function () {
+  sidebar.classList.remove("openbar");
+});
+
+// loader
+
+gsap.to(".loader", {
+  top: "-100%",
+  opacity: 0,
+});
